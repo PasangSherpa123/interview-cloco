5c784d9ffc16102f3ab9fb3d1d9d7d2d2f10d554
PasangSherpa123 <pasangsherpa12580@gmail.com>
2025-01-10 06:32:41 +0545

    connect to database using pg

--- server/index.js
+++ server/index.js
@@ -1,25 +1,46 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import express from "express";
+import dotenv from "dotenv";
+import pkg from "pg";
+const { Client } = pkg;
 
-// Load environment variables
 dotenv.config();
 
 const app = express();
 
-// Middleware for parsing JSON
 app.use(express.json());
 
-// Example route
-app.get('/', (req, res) => {
-    res.send('Welcome to the Express.js backend!');
+app.get("/", (req, res) => {
+  res.send("Welcome to the Express.js backend!");
 });
 
-// Import routes
-const exampleRoutes = require('./routes/exampleRoutes');
-app.use('/api/example', exampleRoutes);
+// const { Pool } = require('pg');
+console.log({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+client
+	.connect()
+	.then(() => {
+		console.log('Connected to PostgreSQL database');
+	})
+	.catch((err) => {
+		console.error('Error connecting to PostgreSQL database', err);
+	});
+
+// testConnection();
 
 // Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
