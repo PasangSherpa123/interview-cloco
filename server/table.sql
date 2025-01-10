e38842f5357e5600f058588c4f0f28c8aa3110da
PasangSherpa123 <pasangsherpa12580@gmail.com>
2025-01-10 06:51:49 +0545

    added tables to the database

---
+++ server/table.sql
@@ -0,0 +1,37 @@
+CREATE TABLE user (
+    id SERIAL PRIMARY KEY,           
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(500) NOT NULL,
+    phone VARCHAR(20) UNIQUE NOT NULL,
+    dob DATE,
+    gender ENUM('m', 'f', 'o') DEFAULT 'o',
+    address VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW() ON UPDATE NOW()
+);
+
+
+
+CREATE TABLE artist (
+    id SERIAL PRIMARY KEY,           
+    name VARCHAR(255) NOT NULL,
+    dob DATE,
+    gender ENUM('m', 'f', 'o') DEFAULT 'o',
+    address VARCHAR(255) NOT NULL,
+    first_release_year SMALLINT NOT NULL
+    no_of_albums_release INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW() ON UPDATE NOW()
+);
+
+CREATE TABLE music (
+    id SERIAL PRIMARY KEY,
+    artist_id INT NOT NULL REFERENCES artist(id) ON DELETE CASCADE,
+    title VARCHAR(255) NOT NULL,
+    album_name VARCHAR(255),
+    genre ENUM('rnb', 'country', 'classic', 'rock', 'jazz') NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW() ON UPDATE NOW()
+);
